17f17be5f7cfe237b365cc084886a69f3c47c756
nvinzens <vinzensnico@hotmail.com>
2017-10-26 14:58:27 +0200

    changed startupscript to install napalm completely

--- saltstack/scripts/install_dependencies.sh
+++ saltstack/scripts/install_dependencies.sh
@@ -3,4 +3,6 @@ sudo apt-get update && sudo apt-get install -y --force-yes libssl-dev libffi-dev
 sudo apt-get install -y python-pip python-dev build-essential
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
-sudo pip install napalm-ios
+sudo pip install setuptools
+sudo pip install --upgrade setuptools
+sudo pip install napalm
