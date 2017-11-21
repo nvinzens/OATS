1d35719077eaffeb714faf6d59d1010b88367c95
nvinzens <nvinzens@hsr.ch>
2017-11-21 09:40:07 +0100

    Revert "add interface_neighbor()"
    This reverts commit 100ed2d3ab155b6379832760a1e1b0c97d50d797.

--- saltstack/salt/_runners/tshoot.py
+++ saltstack/salt/_runners/tshoot.py
@@ -1,29 +1,25 @@
-import pymongo
-from pymongo import Mongoclient
 
-
-def ifdown(host, origin_ip, yang_message, error, tag):
+def ifdown(minion, origin_ip, yang_message, error, tag):
     '''
     Execution function to ping and determine if a state should be invoked.
     '''
     comment = ''
-    conf = 'No changes'
-    success = False
     yang_message = YangMessage(yang_message)
     interface = yang_message.getInterface()
-    interface_neighbor = __get_interface_neighbor(host, interface)
-
-    pingable = __ping('R11', host)
+    destination = '172.16.12.1'
+    success = False
+    conf = ''
+    pingable = __ping('R11', destination)
     if pingable:
-        conf = __if_noshutdown(host, interface)
+        conf = __if_noshutdown(minion, interface)
         success = True
-        comment = ("Config on " + host + " for Interface " + interface
-                   + " changed from down to up")
+        comment = ("Config on " + minion + " for Interface " + interface
+                     + " changed from down to up")
         __post_slack(comment)
     if not pingable:
         success = False
         __post_slack('Interface ' + interface + ' on host '
-                     + host + ' down')
+                   + minion + ' down')
         comment = "Could not restore connectivity - Slack Message sent"
 
     return {
@@ -66,12 +62,6 @@ def __check_device_connectivity(neighbors, host):
         connected = __ping(neighbors, host)
         if connected:
             return connected
-
-def __get_interface_neighbor(host, interface):
-    links = db.network.find_one({'host_name': host})['connections']
-    for link in links:
-        if link['interface'] == interface:
-            return link['neighbor']
 
 class YangMessage(object):
     def __init__(self, yang_message):
