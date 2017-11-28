c7b23f575305a5f469d357f43c0726ff33bc6de6
rjoehl <rjoehl@hsr.ch>
2017-11-28 14:18:23 +0100

    Change oatshelper Description

--- saltstack/oatshelpers/setup.py
+++ saltstack/oatshelpers/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='oatshelpers',
       version='0.11',
-      description='Helper functions for the OATS System',
+      description='Helper functions for OATS, includes db access functions which require MongoDB ',
       url='http://github.com/rjoehl',
-      author='Raphael Joehl',
+      author='Raphael Joehl, Nico Vinzens',
       author_email='rjoehl@hsr.ch',
       license='',
       packages=find_packages(exclude=('tests', 'docs'))
