16abad1a43e4b2c4e71ecfebc5189fe084948471
nvinzens <nvinzens@hsr.ch>
2017-11-30 17:21:32 +0100

    fix ping

--- saltstack/salt/_runners/oats.py
+++ saltstack/salt/_runners/oats.py
@@ -171,12 +171,13 @@ def ping(source, destination, case=None, check_connectivity=False):
     :return: The results of the ping. Will be empty if the ping wasn't successful.
     '''
     if check_connectivity:
-        ping_result = __salt__['salt.execute'](source, 'net.ping', destination=get_vrf_ip(destination), vrf='mgmt')
+        vrf_dest = {'destination': get_vrf_ip(destination), 'vrf': 'mgmt'}
+        ping_result = __salt__['salt.execute'](source, 'net.ping', kwarg=vrf_dest)
         update_case(case, solution='Ping from ' + source + ' to ' + destination + '. Result: ' + str(
             bool(ping_result)))
         return ping_result[source]['out']['success']['results']
     else:
-        ping_result = __salt__['salt.execute'](source, 'net.ping', destination=destination)
+        ping_result = __salt__['salt.execute'](source, 'net.ping', {destination})
         update_case(case, solution ='Ping from ' + source + ' to ' + destination + '. Result: ' + str(bool(ping_result)) + ' //always true in lab env')
     return ping_result[source]['out']['success']['results']
 
@@ -204,7 +205,7 @@ def if_noshutdown(host, interface, case=None):
     '''
     template_name = 'noshutdown_interface'
     template_source = 'interface ' + interface + '\n  no shutdown\nend'
-    config = {'template_name': template_name,'template_source': template_source}
+    config = {'template_name': template_name, 'template_source': template_source}
     update_case(case, solution ='Trying to  apply no shutdown to interface ' + interface + '.')
     return __salt__['salt.execute'](host, 'net.load_template', kwarg=config)
 
