7a1543ff607871311770f4b948849b7ff70ec715
nvinzens <nvinzens@hsr.ch>
2017-11-30 16:37:07 +0100

    refactoring #2

--- saltstack/salt/_runners/tshoot.py
+++ saltstack/salt/_runners/tshoot.py
@@ -6,7 +6,7 @@ import json
 import string
 import random
 from enum import Enum
-from oats import oats
+import oats
 
 
 # Constants
@@ -22,40 +22,38 @@ def ifdown(host, origin_ip, yang_message, error, tag):
     '''
     conf = 'No changes'
     success = False
-    interface = __salt__['salt.cmd'](fun='oats.get_interface', yang_message=yang_message)
+    interface = oats.get_interface(yang_message)
     comment = 'Interface down status on host ' + host + ' detected. '
-    current_case = __salt__['salt.cmd'](fun='oats.create_case', error=error, host=host, status='solution_deployed')
-    interface_neighbor = __salt__['salt.cmd'](fun='oats.get_interface_neighbor',host=host, interface=interface, case=current_case)
+    current_case = oats.create_case(error, host, status='solution_deployed')
+    interface_neighbor = oats.get_interface_neighbor(host, interface, case=current_case)
 
-    neighbors = __salt__['salt.cmd'](fun='oats.get_neighbors', host=interface_neighbor, case=current_case)
-    device_up = __salt__['salt.cmd'](fun='oats.check_device_connectivity', neighbors=neighbors, host=interface_neighbor, case=current_case)
+    neighbors = oats.get_neighbors(interface_neighbor, case=current_case)
+    device_up = oats.check_device_connectivity(neighbors, interface_neighbor, current_case)
 
     if device_up:
         # cycle affected interface
-        __salt__['salt.cmd'](fun='oats.if_shutdown', host=host, interface=interface, case=current_case)
-        conf = __salt__['salt.cmd'](fun='oats.if_noshutdown', host=host, interface=interface, case=current_case)
+        oats.if_shutdown(host, interface, case=current_case)
+        conf = oats.if_noshutdown(host, interface, case=current_case)
         # check if cycle was successful
-        success = __salt__['salt.cmd'](fun='oats.ping', source=host, destination=interface_neighbor, case=current_case)
+        success = oats.ping(host, interface_neighbor, case=current_case)
         if success:
             success = True
             comment += ('Config for Interface '
                        + interface + ' automatically changed from down to up')
             # TODO: remove? only useful for debugging
-            __salt__['salt.cmd'](fun='oats.post_slack', message=comment, case=current_case)
-            __salt__['salt.cmd'](fun='oats.close_case', case_id=current_case)
+            oats.post_slack(comment, case=current_case)
+            oats.close_case(current_case)
         else:
-            __salt__['salt.cmd'](fun='oats.update_case', case_id=current_case, solution=error +
-                                                    'could not get resolved. Technician needed.', status=oats.Status.ONHOLD.value)
+            oats.update_case(current_case, solution=error + 'could not get resolved. Technician needed.', status=oats.Status.ONHOLD.value)
             comment = ('Could not fix down status of ' + interface + ' on host'
                        + host + ' .')
-            __salt__['salt.cmd'](fun='oats.post_slack', comment=comment, case=current_case)
+            oats.post_slack(comment, case=current_case)
     if not device_up:
         # TODO: powercycle, check power consumation
         success = False
-        __salt__['salt.cmd'](fun='oats.update_case', case_id=current_case, solution ='Device ' + interface_neighbor +
-                                                    ' is unreachable. Technician needed.', status=oats.Status.ONHOLD.value)
+        oats.update_case(current_case, solution ='Device ' + interface_neighbor + ' is unreachable. Technician needed.', status=oats.Status.ONHOLD.value)
         comment += 'Interface ' + interface + ' on host '+ host + ' down. Neighbor ' + interface_neighbor + ' is down.'
-        __salt__['salt.cmd'](fun='oats.post_slack', comment=comment, case=current_case)
+        oats.post_slack(comment, case=current_case)
         comment += ' Could not restore connectivity - Slack Message sent.'
 
     return {
