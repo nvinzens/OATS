2cf07579578183334a25e7e92f0a11589e8b0285
nvinzens <nvinzens@hsr.ch>
2017-12-02 13:15:32 +0100

    minor fix

--- saltstack/oatsdbhelpers/oats/oats.py
+++ saltstack/oatsdbhelpers/oats/oats.py
@@ -290,4 +290,4 @@ def get_interface(error, yang_message):
         return yang_message['interfaces']['interface'].popitem()[0]
     if error == 'OSPF_NEIGHBOR_DOWN':
         interfaces = yang_message['network-instances']['network-instance']['global']['protocols']['protocol']['ospf']['ospfv2']['areas']['area']['area']
-        return interfaces['interface'].popitem()[0]
+        return interfaces['interfaces']['interface'].popitem()[0]
