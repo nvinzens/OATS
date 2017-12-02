dfbdc0f85bfcc0b1e33a71b486193528e4bcc890
nvinzens <nvinzens@hsr.ch>
2017-12-02 13:02:18 +0100

    add missing param

--- napalm/scripts/client.py
+++ napalm/scripts/client.py
@@ -66,7 +66,6 @@ def __get_interface_status(yang_message):
 
 
 def __get_ospf_change_reason(yang_message):
-    global cache
     for k, v in sorted(yang_message.items()):
         if k == 'state':
             if v['adjacency-state-change-reason-message'] == 'Dead timer expired':
@@ -85,7 +84,7 @@ def __send_salt_async(yang_message, minion, origin_ip, tag, message_details, err
     if optional_arg:
         # make sure cache gets initialized before other threads try to access it
         lock.acquire()
-        interface = oats.get_interface(yang_message)
+        interface = oats.get_interface(error, yang_message)
         root_host = oats.get_interface_neighbor(minion, interface, case=current_case)
         n_of_neighbors = len(oats.get_ospf_neighbors(root_host, case=current_case))
         try:
