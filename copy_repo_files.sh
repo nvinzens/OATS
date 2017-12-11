c923ea1166d22cac474e2152b98577e42e629fc4
nvinzens <vinzensnico@hotmail.com>
2017-12-11 14:29:25 +0100

    update copy_files script

--- copy_repo_files.sh
+++ copy_repo_files.sh
@@ -2,10 +2,12 @@
 # Copies the files from the SA_AT Git repo to the right folders
 echo Start of the Script
 echo Copy Saltstack folder
+mkdir /etc/napalm
+mkdir /srv/saltstack
 cp -r ./saltstack/. /srv/saltstack
-cp -r ./napalm/. /etc/napalm
+cp ./napalm/logs/conf/napalm-logs.conf /etc/napalm/logs
+cp -r ./napalm/scripts/. /etc/napalm/scripts
 cp -r ./saltstack/etc/. /etc/salt
-cp -r ./napalm/logs/conf/napalm-logs.conf /etc/napalm/logs/
 cp -r ./napalm/logs/ios/. /usr/local/lib/python2.7/dist-packages/napalm_logs/config/ios/
 echo all finished
 
