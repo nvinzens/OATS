14a0d65acba7a9b20989daffa05f5c2f628f8fe5
nvinzens <vinzensnico@hotmail.com>
2018-04-15 11:03:08 +0200

    update kafka consumer for buffer client

--- kafka/out-discards-events_consumer.py
+++ kafka/out-discards-events_consumer.py
@@ -1,6 +1,25 @@
 from kafka import KafkaConsumer
+from salt_event import send_salt_event
+
+def __extract_data(message):
+    host = None
+    timestamp = None
+    data = None
+    for key, value in message._asdict().items():
+        if key == 'key':
+            host = value
+        elif key == 'timestamp':
+            timestamp = value
+        elif key == 'value':
+            data = value
+    return host, timestamp, data
 
 consumer = KafkaConsumer('out-discards-events')
+
 for msg in consumer:
-    print (msg)
+    print msg
+    host, timestamp, data = __extract_data(msg)
+    send_salt_event(data, host, timestamp)
+
+
 
--- kafka/streams-pipe-output_consumer.py
+++ kafka/streams-pipe-output_consumer.py
@@ -1,6 +1,11 @@
 from kafka import KafkaConsumer
+import json
+import msgpack
 
-consumer = KafkaConsumer('streams-pipe-output')
+consumer = KafkaConsumer(value_deserializer=lambda m: json.loads(m))
+consumer.subscribe(['streams-pipe-output'])
+
 for msg in consumer:
-    print (msg)
-
+    for key, value in msg._asdict().items():
+        print (key)
+        print (value)
