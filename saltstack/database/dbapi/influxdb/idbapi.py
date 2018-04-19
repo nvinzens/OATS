74fed452b8e2261e547668ec6906e304e8af25dd
rjoehl <rjoehl@hsr.ch>
2018-04-19 11:19:13 +0200

    Add db connect to influx api

--- saltstack/database/dbapi/influxdb/idbapi.py
+++ saltstack/database/dbapi/influxdb/idbapi.py
@@ -4,9 +4,28 @@ from influxdb import InfluxDBClient
 # client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
 
 
-def write(measurement, host, region, value, time=None):
+def connect_influx_client(host=None, port=None, user=None, password=None, dbname=None):
+    if not host:
+        host = 'localhost'
+    if not port:
+        port = 8086
+    if not user:
+        user = 'root'
+    if not password:
+        password = 'root'
+    if not dbname:
+        dbname = 'example'
+    client = InfluxDBClient(host, port, user, password, dbname)
 
-    client = InfluxDBClient(database='timedb')
+    return client
+
+
+def write(measurement, host, region, value, time=None, db=None):
+
+    if not db:
+        db = 'timedb'
+
+    client = connect_influx_client(host=None, port=None, user=None, password=None, dbname=db)
 
     measure = measurement
     device = host
