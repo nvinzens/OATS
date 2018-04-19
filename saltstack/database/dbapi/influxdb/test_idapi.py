26401d7eaf64b0aa1dacc652bdf53f8e4065b7d5
rjoehl <rjoehl@hsr.ch>
2018-04-19 14:44:47 +0200

    Add Test for Infulxdb api

--- saltstack/database/dbapi/influxdb/test_idapi.py
+++ saltstack/database/dbapi/influxdb/test_idapi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 from influxdb import InfluxDBClient
+import datetime
 
 # client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
 
@@ -73,3 +74,79 @@ def write(measurement, host, interface, region, value, time=None, db=None, clien
     client.close()
 
     return success
+
+
+def test_connect():
+
+    db = 'test'
+
+    client = connect_influx_client(dbname=db)
+
+    client.create_database(db)
+
+    point = [
+        {
+            "measurement": 'test',
+            "tags": {
+                "host": 'test_dev',
+                "interface": 'test_port',
+                "region": 'test_reg'
+            },
+            "fields": {
+                "value": 1
+            }
+        }
+    ]
+
+    test_val = client.write_points(point)
+
+    client.drop_database(db)
+
+    client.close()
+
+    assert test_val is True
+
+
+def test_write_no_time():
+
+    db = 'test'
+    measure = 'test_ms'
+    host = 'test_host'
+    interface = 'test_port'
+    reg = 'test_reg'
+    val = 1
+
+    cl = connect_influx_client(dbname=db)
+
+    cl.create_database(db)
+
+    success = write(measurement=measure, host=host, interface=interface, region=reg, value=val, time=None, db=db, client=cl)
+
+    cl.drop_database(db)
+
+    cl.close()
+
+    assert success is True
+
+
+def test_write_with_time():
+
+    db = 'test'
+    measure = 'test_ms'
+    host = 'test_host'
+    interface = 'test_port'
+    reg = 'test_reg'
+    time = datetime.datetime.utcnow()
+    val = 1
+
+    cl = connect_influx_client(dbname=db)
+
+    cl.create_database(db)
+
+    success = write(measurement=measure, host=host, interface=interface, region=reg, value=val, time=time, db=db, client=cl)
+
+    cl.drop_database(db)
+
+    cl.close()
+
+    assert success is True
