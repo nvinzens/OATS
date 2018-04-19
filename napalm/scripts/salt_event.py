1ab881da8602580204c5834f6de8fdfdf24860b0
nvinzens <vinzensnico@hotmail.com>
2018-04-19 09:42:23 +0200

    init systemd

--- napalm/scripts/salt_event.py
+++ napalm/scripts/salt_event.py
@@ -2,6 +2,47 @@
 from __future__ import with_statement
 import salt.utils.event
 import salt.client
+import yaml
+
+
+class SaltEventConfig:
+
+    def __init__(self, yaml_file_name):
+        file = open(yaml_file_name)
+        self.config = yaml.load(file)
+
+    def get_host_configs(self):
+        hosts = []
+        for host in self.config['hosts']:
+            hosts.append(host)
+        return hosts
+
+    def get_subscriptions(self, host_config):
+        subs = []
+        for sub in host_config['subscriptions']:
+            subs.append(sub)
+        return subs
+
+    def get_host(self, host_config):
+        return host_config['host']
+
+    def get_port(self, host_config):
+        return host_config['port']
+
+    def get_username(self, host_config):
+        return host_config['username']
+
+    def get_password(self, host_con fig):
+        return host_config['password']
+
+    def get_kafka_topic(self, subscription):
+        return subscription['subscription']['kafka_publish_topic']
+
+    def get_publish_period(self, subscription):
+        return subscription['subscription']['period']
+
+    def get_xpath(self, subscription):
+        return subscription['subscription']['xpath']
 
 
 def send_salt_event(yang_message, minion, origin_ip, tag, message_details, error, optional_arg, case=None):
