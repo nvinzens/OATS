1025a7a0ade27daa7ec4fda98a8f2802c8091153
rjoehl <rjoehl@hsr.ch>
2018-05-01 14:11:55 +0200

    added missing tavis refs, influx event write frame

--- saltstack/database/oatsinflux/oatsinflux/oatsinflux.py
+++ saltstack/database/oatsinflux/oatsinflux/oatsinflux.py
@@ -20,6 +20,45 @@ def connect_influx_client(host=None, port=None, user=None, password=None, dbname
     return client
 
 
+def write_event(host, hostip, timestamp, type, event_name, severity, data, db=None, client=None):
+
+    if not db:
+        db = 'timedb'
+    if not client:
+        client = connect_influx_client(host=None, port=None, user=None, password=None, dbname=db)
+
+    try:
+        if type == 'syslog':
+            __write_syslog(client)
+        elif type == 'api':
+            __write_api(client)
+        elif type == 'netflow':
+            __write_netflow(client)
+        elif type == 'streaming-telemetry':
+            __write_stream(client)
+        else:
+            raise ValueError('Invalid Event Type')
+    except Exception as error:
+        print('Caught this error: ' + repr(error))
+
+
+def __write_syslog(client):
+
+    return 0
+
+def __write_api(client):
+
+    return 0
+
+def __write_netflow(client):
+
+    return 0
+
+def __write_stream(client):
+
+    return 0
+
+
 def write(measurement, host, interface, region, value, time=None, db=None, client=None):
 
     if not db:
