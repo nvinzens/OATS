39a93c0f2849eec0bd94bf0a38402a66757757ff
rjoehl <rjoehl@hsr.ch>
2018-05-03 13:21:21 +0200

    DB migration

--- saltstack/salt/_runners/oatssalthelpers.py
+++ saltstack/salt/_runners/oatssalthelpers.py
@@ -1,4 +1,6 @@
 from oats import oatsdbhelpers
+from oatspsql import oatspsql
+from oatsnb import oatsnb
 import time
 import salt.config
 import salt.utils.event
@@ -20,8 +22,8 @@ def post_slack(message, case=None):
     api_key = __load_api_key('/etc/salt/master')
     solutions = 'No linked workflow.'
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Workflow finished. Case-ID: ' + case, status=oatsdbhelpers.Status.ONHOLD.value)
-    solutions = oatsdbhelpers.get_solutions_as_string(case)
+        oatspsql.update_case(case, solution='Workflow finished. Case-ID: ' + case, status=oatspsql.Status.ONHOLD.value)
+    solutions = oatspsql.get_solutions_as_string(case)
     message += "\nExecuted workflow:\n" + solutions
     return __salt__['salt.cmd'](fun='slack.post_message', channel=channel, message=message, from_name=user, api_key=api_key)
 
@@ -42,16 +44,16 @@ def ping(source, destination, case=None, check_connectivity=False):
     :return: The results of the ping as a dict. Will be empty if the ping wasn't successful.
     '''
     if check_connectivity:
-        vrf_dest = {'destination': oatsdbhelpers.get_vrf_ip(destination), 'vrf': 'mgmt'}
+        vrf_dest = {'destination': oatsnb.get_vrf_ip(destination), 'vrf': 'mgmt'}
         ping_result = __salt__['salt.execute'](source, 'net.ping', kwarg=vrf_dest)
         if case is not None:
-            oatsdbhelpers.update_case(case, solution='Ping from ' + source + ' to ' + destination + '. Result: ' + str(
-            bool(ping_result)))
+            oatspsql.update_case(case, solution='Ping from ' + source + ' to ' + destination + '. Result: ' + str(
+                bool(ping_result)))
         return ping_result[source]['out']['success']['results']
     else:
         ping_result = __salt__['salt.execute'](source, 'net.ping', {destination})
         if case is not None:
-            oatsdbhelpers.update_case(case, solution ='Ping from ' + source + ' to ' + destination + '. Result: ' + str(bool(ping_result)) + ' //always true in lab env')
+            oatspsql.update_case(case, solution='Ping from ' + source + ' to ' + destination + '. Result: ' + str(bool(ping_result)) + ' //always true in lab env')
     return ping_result[source]['out']['success']['results']
 
 
@@ -70,7 +72,7 @@ def if_noshutdown(host, interface, case=None):
     template_source = 'interface ' + interface + '\n  no shutdown\nend'
     config = {'template_name': template_name, 'template_source': template_source}
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Trying to apply no shutdown to interface {0} on host {1}.'.format(interface, host))
+        oatspsql.update_case(case, solution='Trying to apply no shutdown to interface {0} on host {1}.'.format(interface, host))
     return __salt__['salt.execute'](host, 'net.load_template', kwarg=config)
 
 
@@ -89,7 +91,7 @@ def if_shutdown(host, interface, case=None):
     template_source = 'interface {0}\n  shutdown\nend'.format(interface)
     config = {'template_name': template_name,'template_source': template_source}
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Trying to apply shutdown to interface {0} on host {1}.'.format(interface, host))
+        oatspsql.update_case(case, solution='Trying to apply shutdown to interface {0} on host {1}.'.format(interface, host))
     return __salt__['salt.execute'](host, 'net.load_template', kwarg=config)
 
 
@@ -107,8 +109,9 @@ def ospf_shutdown(minion, process_number, case=None):
     template_source = 'router ospf {0}\n  shutdown\nend'.format(process_number)
     config = {'template_name': template_name, 'template_source': template_source}
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Trying to apply shutdown to OSPF process {0}.'.format(process_number))
+        oatspsql.update_case(case, solution='Trying to apply shutdown to OSPF process {0}.'.format(process_number))
     return __salt__['salt.execute'](minion, 'net.load_template', kwarg=config)
+
 
 def ospf_noshutdown(minion, process_number, case=None):
     '''
@@ -124,8 +127,9 @@ def ospf_noshutdown(minion, process_number, case=None):
     template_source = 'router ospf {0}\n  no shutdown\nend'.format(process_number)
     config = {'template_name': template_name, 'template_source': template_source}
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Trying to apply no shutdown to OSPF process {0}.'.format(process_number))
+        oatspsql.update_case(case, solution='Trying to apply no shutdown to OSPF process {0}.'.format(process_number))
     return __salt__['salt.execute'](minion, 'net.load_template', kwarg=config)
+
 
 def check_device_connectivity(neighbors, host, case=None):
     '''
@@ -140,7 +144,7 @@ def check_device_connectivity(neighbors, host, case=None):
     for neighbor in neighbors:
         connected = ping(neighbor, host, check_connectivity=True)
         if case is not None:
-            oatsdbhelpers.update_case(case,
+            oatspsql.update_case(case,
                                       solution='Checking connectivity from {0} to {1}. Result: {2}'
                                       .format(neighbor, host,str(bool(connected))))
         if connected:
@@ -170,13 +174,13 @@ def count_event(tag, error, amount, wait=10, case=None):
     counter = 0
     timeout = time.time() + wait
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Waiting for {0} {1} events.'.format(amount, error))
+        oatspsql.update_case(case, solution='Waiting for {0} {1} events.'.format(amount, error))
     while time.time() < timeout:
         if event.get_event(wait=3, tag=tag):
             counter += 1
     success = counter >= amount
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Result: {0} events. Wait success: {1}.'.format(counter, success))
+        oatspsql.update_case(case, solution='Result: {0} events. Wait success: {1}.'.format(counter, success))
     return success
 
 
@@ -190,11 +194,11 @@ def wait_for_event(tag, error, amount, wait=10, case=None):
         opts=opts)
     data = event.get_event(wait=wait, tag=tag)
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Waiting for {0} event...'.format(tag))
+        oatspsql.update_case(case, solution='Waiting for {0} event...'.format(tag))
     if data:
         if case is not None:
-            oatsdbhelpers.update_case(case, solution='Received {0} event: Wait was successful.'.format(tag))
+            oatspsql.update_case(case, solution='Received {0} event: Wait was successful.'.format(tag))
         return True
     if case is not None:
-        oatsdbhelpers.update_case(case, solution='Wait timeout: did not receive {0} event. Troubleshooting failed.'.format(tag))
+        oatspsql.update_case(case, solution='Wait timeout: did not receive {0} event. Troubleshooting failed.'.format(tag))
     return False
--- saltstack/salt/_runners/tshoot.py
+++ saltstack/salt/_runners/tshoot.py
@@ -1,4 +1,6 @@
 from oats import oatsdbhelpers
+from oatspsql import oatspsql
+from oatsnb import oatsnb
 import oatssalthelpers
 from multiprocessing.pool import ThreadPool
 import time
@@ -25,10 +27,10 @@ def ifdown(host, yang_message, error, tag, current_case=None):
     interface = oatsdbhelpers.get_interface(error, yang_message)
     comment = 'Interface down status on host ' + host + ' detected. '
     if current_case is None or current_case == 'None':
-        current_case = oatsdbhelpers.create_case(error, host, solution='Case created in salt: tshoot.ifdown().')
-    interface_neighbor = oatsdbhelpers.get_interface_neighbor(host, interface, case=current_case)
+        current_case = oatspsql.create_case(error, host, solution='Case created in salt: tshoot.ifdown().')
+    interface_neighbor = oatsnb.get_interface_neighbor(host, interface, case=current_case)
 
-    neighbors = oatsdbhelpers.get_neighbors(interface_neighbor, case=current_case)
+    neighbors = oatsnb.get_neighbors(interface_neighbor, case=current_case)
     device_up = oatssalthelpers.check_device_connectivity(neighbors, interface_neighbor, case=current_case)
 
     if device_up:
@@ -43,16 +45,16 @@ def ifdown(host, yang_message, error, tag, current_case=None):
                        + interface + ' automatically changed from down to up')
             # TODO: remove, only useful for debugging
             oatssalthelpers.post_slack(comment, case=current_case)
-            oatsdbhelpers.close_case(current_case)
+            oatspsql.close_case(current_case)
         else:
-            oatsdbhelpers.update_case(current_case, solution=error + 'could not get resolved. Technician needed.', status=oatsdbhelpers.Status.ONHOLD.value)
+            oatspsql.update_case(current_case, solution=error + 'could not get resolved. Technician needed.', status=oatspsql.Status.ONHOLD.value)
             comment = ('Could not fix down status of ' + interface + ' on host'
                        + host + ' .')
             oatssalthelpers.post_slack(comment, case=current_case)
     if not device_up:
         # TODO: powercycle, check power consumation
         success = False
-        oatsdbhelpers.update_case(current_case, solution ='Device ' + interface_neighbor + ' is unreachable. Technician needed.', status=oatsdbhelpers.Status.ONHOLD.value)
+        oatspsql.update_case(current_case, solution ='Device ' + interface_neighbor + ' is unreachable. Technician needed.', status=oatspsql.Status.ONHOLD.value)
         comment += 'Interface ' + interface + ' on host '+ host + ' down. Neighbor ' + interface_neighbor + ' is down.'
         oatssalthelpers.post_slack(comment, case=current_case)
         comment += ' Could not restore connectivity - Slack Message sent.'
@@ -85,10 +87,10 @@ def ospf_nbr_down(host, yang_message, error, tag, process_number, current_case):
     pool = ThreadPool(processes=1)
     comment = 'OSPF neighbor down status on host {0} detected.'.format(host)
     if current_case is None or current_case == 'None':
-        current_case = oatsdbhelpers.create_case(error, host, solution='Case created in salt: tshoot.ospf_nbr_down().')
+        current_case = oatspsql.create_case(error, host, solution='Case created in salt: tshoot.ospf_nbr_down().')
     interface = oatsdbhelpers.get_interface(error, yang_message)
-    interface_neighbor = oatsdbhelpers.get_interface_neighbor(host, interface, case=current_case)
-    n_of_neighbors = len(oatsdbhelpers.get_ospf_neighbors(interface_neighbor, case=current_case))
+    interface_neighbor = oatsnb.get_interface_neighbor(host, interface, case=current_case)
+    n_of_neighbors = len(oatsnb.get_ospf_neighbors(interface_neighbor, case=current_case))
     oatssalthelpers.ospf_shutdown(interface_neighbor, process_number, case=current_case)
     async_result = pool.apply_async(
         oatssalthelpers.wait_for_event, ('napalm/syslog/*/OSPF_NEIGHBOR_UP/ospf_nbrs_up',
@@ -97,12 +99,12 @@ def ospf_nbr_down(host, yang_message, error, tag, process_number, current_case):
     conf = oatssalthelpers.ospf_noshutdown(interface_neighbor, process_number, case=current_case)
     success = async_result.get()
     if success:
-        oatsdbhelpers.update_case(current_case, 'Successfully restarted OSPF process on host {0}.'
-                                    .format(interface_neighbor), oatsdbhelpers.Status.DONE.value)
+        oatspsql.update_case(current_case, 'Successfully restarted OSPF process on host {0}.'
+                                    .format(interface_neighbor), oatspsql.Status.DONE.value)
         comment += ' OSPF process restarted successfully.'
     else:
-        oatsdbhelpers.update_case(current_case, 'Unable to restart OSPF process on host {0}. Host might be offline.'
-                                       '. Technician needed.'.format(interface_neighbor), oatsdbhelpers.Status.ONHOLD.value)
+        oatspsql.update_case(current_case, 'Unable to restart OSPF process on host {0}. Host might be offline.'
+                                       '. Technician needed.'.format(interface_neighbor), oatspsql.Status.ONHOLD.value)
     slack_post = oatssalthelpers.post_slack(comment, case=current_case)
 
     ret = {
@@ -119,7 +121,7 @@ def ospf_nbr_down(host, yang_message, error, tag, process_number, current_case):
 
 def out_discards_exceeded(data, host, timestamp, current_case=None):
     if current_case is None or current_case == 'None':
-        current_case = oatsdbhelpers.create_case("OUT_DISCARDS_EXCEEDED", host, solution='Case created in salt: tshoot.out_discards_exceeded().')
+        current_case = oatspsql.create_case("OUT_DISCARDS_EXCEEDED", host, solution='Case created in salt: tshoot.out_discards_exceeded().')
     eventTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
     comment = "Discarded pakets on host {0} on egress interface {1} exceeds threshhold. Time of Event: {2}".format(host, data['ifaceName'], eventTime)
     ret = oatssalthelpers.post_slack(comment, case=current_case)
