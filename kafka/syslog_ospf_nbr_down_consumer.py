beed74aa414d7c28bef6b281704e8ea35b9bcb68
nvinzens <nvinzens@hsr.ch>
2018-05-10 09:14:19 +0200

    fix ospf_nbr_down event

--- kafka/syslog_ospf_nbr_down_consumer.py
+++ kafka/syslog_ospf_nbr_down_consumer.py
@@ -30,12 +30,12 @@ for msg in consumer:
 
     # only start aggregation once the dead timer is expired
     if salt_id == 'dead_timer_expired':
-        event_name = 'syslog/*/OSPF_NEIGHBOR_DOWN/' + salt_id
-        alt_event_name = 'syslog/*/' + event_error + salt_id
+        event_name = 'syslog/*/' + event_error + salt_id
+        alt_event_name = 'syslog/*/' + event_error + 'interface_down'
         n_of_required_events, root_host = utils.get_n_of_events_and_root_host(event_error, host, yang_mess)
 
         thread = Thread(target=oats_correlate.aggregate,
-                        args=(event_msg, host, timestamp, severity, event_error, event_name, salt_id,
+                        args=(event_msg, host, timestamp, severity, event_error, 'syslog', event_name,
                               n_of_required_events, alt_event_name, 10, True))
         thread.daemon = True
         thread.start()
