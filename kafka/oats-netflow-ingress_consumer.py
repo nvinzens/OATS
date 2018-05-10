68842dac326d3641ceb8109eca981663589384b3
nvinzens <nvinzens@hsr.ch>
2018-05-10 13:25:58 +0200

    only send relevant netflow data

--- kafka/oats-netflow-ingress_consumer.py
+++ kafka/oats-netflow-ingress_consumer.py
@@ -13,18 +13,30 @@ for msg in consumer:
     type = 'netflow'
     event_name = 'netflow/*/high_traffic'
     severity = 3
-
+    relevant_data = []
+    big_flow = False
     for list in netflow_data['DataSets']:
+        list = sorted(list)
         for dict in list:
             if dict['I'] == 1:
                 if dict['V'] > 10000:
+                    big_flow = True
                     print ("Packets in detected flow: " + str(dict['V']))
-                    # delay the flow for a bit to make sure it arrives later than the event it is needed in
-                    thread = Thread(target=EventProcessor.process_event,
-                                    args=(netflow_data, host, timestamp, 'netflow', event_name, severity),
-                                    kwargs={'start_tshoot': True, 'db_write': False, 'delay': 5})
-                    thread.daemon = True
-                    thread.start()
+            if big_flow:
+                if dict['I'] == 7:
+                    if dict['V'] > 10000:
+                        print ("Source port of flow: " + dict['V'])
+
+                        # delay the flow for a bit to make sure it arrives later than the event it is needed in
+                        thread = Thread(target=EventProcessor.process_event,
+                                        args=(netflow_data, host, timestamp, 'netflow', event_name, severity),
+                                        kwargs={'start_tshoot': True, 'db_write': False, 'delay': 5})
+                        thread.daemon = True
+                        thread.start()
+                        break
+
+            else:
+                continue
                     
 
 
