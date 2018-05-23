87accecb942d9d1630e23a54383c1f655fee78fa
nvinzens <nvinzens@hsr.ch>
2018-05-23 10:45:22 +0200

    fix event string

--- kafka/oats_kafka_helpers/oats_correlate.py
+++ kafka/oats_kafka_helpers/oats_correlate.py
@@ -122,7 +122,7 @@ def aggregate_distinct(data, host, timestamp, severity, error, sensor_type,
     success = True
     for event in event_names:
         print (event_name, cache[cache_id][event], cache[cache_id][event]['counter'], distinct_events[event])
-        if not cache[cache_id][event]['counter'] >= distinct_events[event]:
+        if not cache[cache_id][event]['counter'] >= distinct_events[event[3:]]:
             success = False
             break
     print success
