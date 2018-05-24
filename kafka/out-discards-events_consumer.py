83febdc3917a2ae073ccab7db39b9c71e8570065
nvinzens <nvinzens@hsr.ch>
2018-05-24 09:08:20 +0200

    add logger

--- kafka/out-discards-events_consumer.py
+++ kafka/out-discards-events_consumer.py
@@ -10,7 +10,7 @@ consumer = KafkaConsumer('out-discards-events')
 for msg in consumer:
     host, timestamp, data = utils.extract_record_data(msg)
     thread = Thread(target=oats_correlate.compress,
-                    args=(data, host, timestamp, 6, "OUT_DISCARDS_EXCEEDED", 'syslog', "out_discard_event",
+                    args=(data, host, timestamp, 2, "OUT_DISCARDS_EXCEEDED", 'syslog', "out_discard_event",
                           10, True))
     thread.daemon = True
     thread.start()
--- telemetry/oats_telemetry.py
+++ telemetry/oats_telemetry.py
@@ -2,25 +2,46 @@ import netconf_telemetry
 import kafka_streams
 from OATSConfig import OATSConfig
 from multiprocessing import Process
+import logging
 
 
 YAML_FILE = '/etc/oats/config.yaml'
 
 
-if __name__ == '__main__':
+def main():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('/var/log/oats/logs')
+    fh.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+
+    logger.addHandler(fh)
+
+    logger.info('Starting oats telemetry client...')
+
+    logger.debug('Reading oats configuration from {0}'.format(YAML_FILE))
     config = OATSConfig(YAML_FILE)
 
     # start kafka streams clients and consumers
     subscriptions = config.get_telemetry_subscriptions()
     for sub in subscriptions:
         if sub.kafka_streams_eval:
+            logger.info('Starting kafka streams for {0} xpath'.format(sub.xpath))
             kafka_streams.start_kafka_streams(sub)
 
     # establish telemetry subscriptions
     host_configs = config.get_host_configs()
     for host_config in host_configs:
+        logger.info("Starting netconf telemetry session(s) for host {0}".format(host_config.hostname))
         p = Process(target=netconf_telemetry.process_host_config, args=(host_config, config))
         p.start()
+
+
+if __name__ == '__main__':
+    main()
 
 
 
